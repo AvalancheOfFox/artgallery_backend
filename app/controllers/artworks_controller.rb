84b75745581e391c9c0a3735c796e997d64ec62d
Ross Perkel <rossperkel@gmail.com>
2019-09-09 18:29:59 -0400

    working version preMonday Dnd

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,17 +1,52 @@
 class ArtworksController < ApplicationController
 
+    before_action :set_artwork, only: [:update, :destroy]
+
     def index
         @artworks = Artwork.all
         render json: ArtworkSerializer.new(@artworks)
     end
 
     def show
-        @artwork = artwork.find(params[:id])
         options = {
             include: [:artist]
         }
-        render json: ArtworkSerializer.new(artwork, options)
+        render json: ArtworkSerializer.new(@artwork, options)
     end
 
-    
+# POST /artworks
+    def create
+        @artwork = Artwork.create(artwork_params)
+            if @artwork.save
+                render json: @artwork, status: :created, location: @artwork
+            else
+                render json: @artwork.errors, status: :unprocessable_entity
+            end
+    end
+
+# PATCH/PUT /artworks/1
+def update
+    if @artwork.update(artwork_params)
+        render json: @artwork
+    else
+        render json: @artwork.errors, status: :unprocessable_entity
+    end
 end
+
+# DELETE /artworks/1
+def destroy
+    @artwork.destroy
+end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_artwork
+        @artwork = Artwork.find(params[:id])
+    end
+    # Only allow a trusted parameter "white list" through.
+    def artwork_params
+        params.require(:artwork).permit(:id, :title, :medium, :source, :artist_id)
+    end
+end
+    
+
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,45 +1,4 @@
 class Artist < ApplicationRecord
     has_many :artworks
-
-    def index
-        @artists = Artist.all
-        render json: @artists
-    end
-
-    def show
-        render json: @artist
-    end
-
-# POST /artists
-def create
-    @artist = Artist.new(artist_params)
-
-    if @artist.save
-        render json: @artist, status: :created, location: @artist
-    else
-        render json: @artist.errors, status: :unprocessable_entity
-    end
-end
-
-    # PATCH/PUT /artists/1
-def update
-    if @artist.update(artist_params)
-        render json: @artist
-    else
-        render json: @artist.errors, status: :unprocessable_entity
-    end
-end
-
-private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_artist
-        @artist = Artist.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def artist_params
-        params.require(:artist).permit(:name)
-    end
-
     
 end
